d60b2b46b6026b9602c9880ebc7e4b6588f167a4
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-04-19 11:57:33 +0700

    Create Poligon.java

---
+++ Pertemuan 4/org/poligon/Poligon.java
@@ -0,0 +1,20 @@
+/*
+	Nama  File	: Poligon.java
+	Pembuat		: Muhammad Dzaki Prasetyo
+	NIM			: 24060121140127
+	Tanggal		: Rabu, 15 Maret 2023
+	Deskripsi	: Representasi dasar dari objek poligon (segi banyak)
+*/
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public void setJumlahSisi(int jumlahSisi){
+		this.jumlahSisi = jumlahSisi;
+	}
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+		
+}
