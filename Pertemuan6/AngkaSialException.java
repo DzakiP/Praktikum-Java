e1cf67b222ccb5177f0baaaf5a7b4c669d62b7ea
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-04-30 23:33:18 +0700

    Create AngkaSialException.java

---
+++ Pertemuan6/AngkaSialException.java
@@ -0,0 +1,13 @@
+/*
+	Nama  File	: AngkaSialException.java
+	Pembuat		: Muhammad Dzaki Prasetyo
+	NIM			: 24060121140127
+	Tanggal		: Kamis, 27 April 2023
+	Deskripsi	: Eksepsi buatan sendiri, menolak masukan angka 13! 
+*/
+
+public class AngkaSialException extends Exception{
+	public AngkaSialException(){
+		super("jangan memasukkan angka 13 karena angka sial !!!");
+	}
+ }
