65edf450a5f5f69bf79d0da04057f2b4786f82a8
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-04-30 23:27:34 +0700

    Create IArea.java

---
+++ Pratikum5/IArea.java
@@ -0,0 +1,14 @@
+/*
+	Nama  File	: IArea.java
+	Pembuat		  : Muhammad Dzaki Prasetyo
+	NIM			    : 24060121140127
+	Tanggal		  : Kamis, 27 April 2023
+	Deskripsi	  : interface yang mendefinisikan kontrak
+				  bahwa setiap kelas yang membuat implementasi
+				  dari interface ini harus memiliki cara untuk 
+				  menghitung luas
+*/
+
+public interface IArea{
+	public double hitungLuas();
+ }
