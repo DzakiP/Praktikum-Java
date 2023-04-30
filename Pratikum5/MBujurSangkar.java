f238b3a693dd86666d3f2485ec3f0ee725926b1b
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-04-30 23:28:17 +0700

    Create MBujurSangkar.java

---
+++ Pratikum5/MBujurSangkar.java
@@ -0,0 +1,22 @@
+/*
+	Nama  File	: MBujurSangkar.java
+	Pembuat		  : Muhammad Dzaki Prasetyo
+	NIM			    : 24060121140127
+	Tanggal		  : Kamis, 27 April 2023
+	Deskripsi	  : kelas yang mengimplementasikan cara menghitung 
+               luas bujur sangkar
+*/
+
+import java.util.Scanner;
+ 
+ class MBujurSangkar{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		BujurSangkar bs = new BujurSangkar();
+		System.out.print("Masukkan sisi bujur sangkar : ");
+		double sisi = scan.nextDouble();
+		System.out.println("Luas bujur sangkar dengan sisi "+sisi+
+					" satuan adalah "+bs.hitungLuas(sisi));
+	}
+ }
+ 
