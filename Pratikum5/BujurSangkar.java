78c4252cebf8e78c2809793f6ad43ce544dc0209
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-04-30 23:26:34 +0700

    Create BujurSangkar.java

---
+++ Pratikum5/BujurSangkar.java
@@ -0,0 +1,14 @@
+/*
+	Nama  File	: BangunDatar.java
+	Pembuat		  : Muhammad Dzaki Prasetyo
+	NIM			    : 24060121140127
+	Tanggal		  : Kamis, 27 April 2023
+	Deskripsi	  : kelas yang membuat implementasi metode abstrak pada bangun datar
+*/
+
+public class BujurSangkar extends BangunDatar{
+	public double hitungLuas(double sisi){
+		luas = sisi*sisi;
+		return luas;
+	}
+ }
