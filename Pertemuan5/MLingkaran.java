301848102a21528a8333a54fd0c5999e3a6cebc2
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-04-30 23:31:49 +0700

    Create MLingkaran.java

---
+++ Pertemuan5/MLingkaran.java
@@ -0,0 +1,15 @@
+/*
+	Nama  File	: MLingkaran.java
+	Pembuat		: Muhammad Dzaki Prasetyo
+	NIM			: 24060121140127
+	Tanggal		: Kamis, 27 April 2023
+	Deskripsi	: implementasi cara menghitung luas lingkaran     
+*/
+
+ public class MLingkaran{
+	public static void main(String[] args){
+		Lingkaran l = new Lingkaran(10.2);
+		System.out.println("Luas lingkaran dengan"+ 
+			"jejari 10.2 satuan adalah "+l.hitungLuas());
+	}
+ }
