c33cc55c15ca67186fc8a06c5b6c3d195dddb6d0
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-04-30 23:33:32 +0700

    Create Asersi1.java

---
+++ Pertemuan6/Asersi1.java
@@ -0,0 +1,19 @@
+/*
+	Nama  File	: Asersi1.java
+	Pembuat		: Muhammad Dzaki Prasetyo
+	NIM			: 24060121140127
+	Tanggal		: Kamis, 27 April 2023
+	Deskripsi	: Program untuk menunjukkan asersi
+*/
+
+public class Asersi1{
+ public static void main(String[] args){
+  int x=0;
+  if(x>0){
+    System.out.println("x bilangan positif");
+  }else{
+    assert(x<0):"ada kesalahan kode";
+	System.out.println("x bilangan negatif");
+  }
+ }
+}
