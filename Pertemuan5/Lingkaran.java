09f9c0889412e60321b9ca7688c3fff843d65571
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-04-30 23:31:20 +0700

    Create Lingkaran.java

---
+++ Pertemuan5/Lingkaran.java
@@ -0,0 +1,24 @@
+/*
+	Nama  File	: Lingkaran.java
+	Pembuat		: Muhammad Dzaki Prasetyo
+	NIM			: 24060121140127
+	Tanggal		: Kamis, 27 April 2023
+	Deskripsi	: kelas implementasi IArea berupa
+				cara menghitung luas lingkaran  
+*/
+
+import static java.lang.Math.PI;
+ 
+class Lingkaran implements IArea{
+	private double jejari;
+	
+	public Lingkaran(double r){
+		jejari = r;
+	}
+	
+	public double hitungLuas(){
+		return PI*jejari*jejari;
+	}
+ }
+ 
+ 
