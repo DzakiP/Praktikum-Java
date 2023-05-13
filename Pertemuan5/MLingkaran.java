19960fc1a59d6a6fb759b3964fca38c959a1524b
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-05-13 11:30:01 +0700

    Update MLingkaran.java

--- Pertemuan5/MLingkaran.java
+++ Pertemuan5/MLingkaran.java
@@ -6,10 +6,14 @@
 	Deskripsi	: implementasi cara menghitung luas lingkaran     
 */
 
+import java.util.Scanner;
+
  public class MLingkaran{
 	public static void main(String[] args){
-		Lingkaran l = new Lingkaran(10.2);
-		System.out.println("Luas lingkaran dengan"+ 
-			"jejari 10.2 satuan adalah "+l.hitungLuas());
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Masukkan jari-jari : ");
+		double jejari = scan.nextDouble();
+		Lingkaran l = new Lingkaran(jejari);
+		System.out.println("Luas lingkaran dengan jejari " +jejari+ " satuan adalah "+l.hitungLuas());
 	}
  }
