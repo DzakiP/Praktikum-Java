2aac8ab87f9d342eb31f2aad5598c4acb57b7ae5
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-06-06 21:02:40 +0700

    Add files via upload

---
+++ pertemuan9/PersonDAO.java
@@ -0,0 +1,9 @@
+package pertemuan9;
+
+import java.beans.IndexedPropertyChangeEvent;
+import java.util.concurrent.ExecutionException;
+
+
+public interface PersonDAO {
+    public void savePerson(Person P) throws Exception;
+}
