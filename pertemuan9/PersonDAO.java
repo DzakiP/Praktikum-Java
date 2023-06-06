034e6250e6705cc6b36b7fa9d9f8ceffb6e8891c
DzakiP <100425237+DzakiP@users.noreply.github.com>
2023-06-06 21:08:17 +0700

    Delete pertemuan9 directory

--- pertemuan9/PersonDAO.java
+++
@@ -1,9 +0,0 @@
-package pertemuan9;
-
-import java.beans.IndexedPropertyChangeEvent;
-import java.util.concurrent.ExecutionException;
-
-
-public interface PersonDAO {
-    public void savePerson(Person P) throws Exception;
-}
